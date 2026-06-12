2389e8307cafa9167ab325882a1a0932179e4844
Shibu TN <tnshibu@yahoo.com>
2026-06-12 20:48:23 +0530

    compile error

--- app/src/main/java/com/vypeensoft/apkbackuprestore/activities/MainActivity.java
+++ app/src/main/java/com/vypeensoft/apkbackuprestore/activities/MainActivity.java
@@ -29,9 +29,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // Apply saved dark mode preference before super.onCreate
-        applySavedNightMode();
-        
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
