39d848968de0c1f713296eb5472ff44a33a4b30b
Shibu TN <tnshibu@yahoo.com>
2026-07-18 22:10:26 +0530

    build changes

--- c.bat
+++ c.bat
@@ -1,1 +1,9 @@
-gradlew assembleDebug
+@echo off
+call sp
+call gradlew assembleDebug
+if %ERRORLEVEL% equ 0 (
+    call cop.bat
+    start cmd /k adb1.bat
+) else (
+    echo [91mBuild FAILED! Copy skipped.[0m
+)
